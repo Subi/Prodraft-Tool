414b891a652d61db1353724c3add6d6151a47735
Subi <negus.dev@gmail.com>
2022-05-14 00:44:59 -0400

    remove useless import

--- components/draft/Draft.js
+++ components/draft/Draft.js
@@ -1,7 +1,6 @@
 import Header from "./Header";
 import ChampionContent from "./ChampionContent";
 import Footer from "./Footer";
-import ChampionContent from "./ChampionContent";
 
 
 function Draft(props) {
